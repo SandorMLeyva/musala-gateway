d295c34b54c2b75996cc28a6f5a15d448f2bd20b
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-11-06 23:01:48 -0800

    Setup configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,26 @@
 import * as express from 'express';
-import { Message } from '@gateway/api-interfaces';
+// import { Message } from '@gateway/api-interfaces';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
-
-const greeting: Message = { message: 'Welcome to api!' };
-
-app.get('/api', (req, res) => {
-  res.send(greeting);
-});
+// Config
+dotenv.config();
 
 const port = process.env.port || 3333;
+const mongoAddr: string = process.env.mongoAddr || "mongodb://localhost";
+const db: string = process.env.db || "gateway";
+
+// Setup app
+mongoose.connect(`${mongoAddr}/${db}`, { useNewUrlParser: true });
+const app = express();
+
+// const greeting: Message = { message: 'Welcome to api!' };
+
+// app.get('/api', (req, res) => {
+//   res.send(greeting);
+// });
+
+// Start server
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
