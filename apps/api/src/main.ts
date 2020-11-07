09d53984b5d1e1beb68a3ff4fc50ca2744170a66
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-11-07 03:33:13 -0800

    Added body-parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,26 +1,27 @@
 import * as express from 'express';
 // import { Message } from '@gateway/api-interfaces';
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import routes from "./app/routes"
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import routes from './app/routes';
+import { json } from 'body-parser';
 
 // Config
 dotenv.config();
 
 const port = process.env.port || 3333;
-const mongoAddr: string = process.env.mongoAddr || "mongodb://localhost";
-const db: string = process.env.db || "gateway";
+const mongoAddr: string = process.env.mongoAddr || 'mongodb://localhost';
+const db: string = process.env.db || 'gateway';
 
 // Setup app
 mongoose.connect(`${mongoAddr}/${db}`, { useNewUrlParser: true });
 const app = express();
 
 // const greeting: Message = { message: 'Welcome to api!' };
-
-app.use("/apiv1", routes);
+app.use(json());
+app.use('/apiv1', routes);
 
 // Start server
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/apiv1` );
+  console.log(`Listening at http://localhost:${port}/apiv1`);
 });
 server.on('error', console.error);
