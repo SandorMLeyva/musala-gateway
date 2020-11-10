f7c433c1059671bc8be10f27cdc6481b1f78260d
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-11-10 03:51:44 -0800

    Added cors to API

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import routes from './app/routes';
 import { json } from 'body-parser';
+import cors from 'cors';
 
 // Setup app
 
 const app = express();
 
+app.use(cors())
 app.use(json());
 app.use('/api/v1', routes);
 
